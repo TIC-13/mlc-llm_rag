8df08f48ccfe56a98f3e2a247f7ce6b86c618c01
Gabriel Tavares <62074916+booleangabs@users.noreply.github.com>
2024-08-12 12:31:54 -0300

    merge: Adding text embedder to the application
    * feat(android): add text embedding process into application pipeline
    
    * feat(android): add embedder model download script
    
    ---------
    
    Authored-by: RafaelB411 <rafaelb411.rb@gmail.com>
    Merged-by: booleangabs <gabrieltavares1303@gmail.com>

--- android/MLCChat/app/src/main/java/ai/luxai/reggaellm/MainActivity.kt
+++ android/MLCChat/app/src/main/java/ai/luxai/reggaellm/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-
+import androidx.compose.ui.platform.LocalContext
 
 class MainActivity : ComponentActivity() {
 
--- android/MLCChat/app/src/main/java/ai/luxai/reggaellm/NavView.kt
+++ android/MLCChat/app/src/main/java/ai/luxai/reggaellm/NavView.kt
@@ -1,7 +1,12 @@
 package ai.luxai.reggaellm
 
+import android.app.Application
+import android.content.Context
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -9,7 +14,11 @@ import androidx.navigation.compose.rememberNavController
 
 @ExperimentalMaterial3Api
 @Composable
-fun NavView(appViewModel: AppViewModel = viewModel()) {
+fun NavView() {
+    val context = LocalContext.current
+    val application = context.applicationContext as Application
+    val appViewModel = AppViewModel(application = application, context = context)
+
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = "home") {
         composable("home") { StartView(navController, appViewModel) }
